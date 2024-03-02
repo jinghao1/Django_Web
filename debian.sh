4e08a86a6002747f18efe637b03ba0abc2c3f152
songjinghao <songjinghao2012@163.com>
2024-03-02 23:48:15 +0800

    init prod

--- debian.sh
+++ debian.sh
@@ -9,4 +9,6 @@ source .env/Dj_web/bin/activate
 apt-get install libmariadb-dev-compat libmariadb-dev libjpeg-dev
 pip3 install -r requirements.txt
 #https://blog.csdn.net/2202_75762088/article/details/134625775
-#python version 3.6.15
+##python version 3.6.15
+# install  docker
+# https://blog.csdn.net/qq_43329216/article/details/134543375
