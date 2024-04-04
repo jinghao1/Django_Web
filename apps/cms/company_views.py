b691bf3e40dcba050734dd1dc0340ad874a35d37
root <root@localhost.localdomain>
2024-04-04 21:52:50 +0800

    update desc

--- apps/cms/company_views.py
+++ apps/cms/company_views.py
@@ -134,32 +134,27 @@ class EditPubCompany(View):
             return render(request, 'cms/company_edit.html', context=context)
  
     def post(self, request):
-        form = AddCourseForm(request.POST)
-        if form.is_valid():
-            # 从form表单中获取数据赋值给model中定义的变量
-            title = form.cleaned_data.get('title')
-            category_id = form.cleaned_data.get('category_id')
-            teacher_id = form.cleaned_data.get('teacher_id')
-            video_url = form.cleaned_data.get('video_url')
-            cover_url = form.cleaned_data.get('cover_url')
-            price = form.cleaned_data.get('price')
-            duration = form.cleaned_data.get('duration')
-            profile = form.cleaned_data.get('profile')
-            # 从form表单中获取的id数据,通过对应模型获取到category和teacher的具体值
-            category = CourseCategory.objects.get(pk=category_id)
-            teacher = Teacher.objects.get(pk=teacher_id)
-
-            # 将以上整理的数据写入数据库的Course模型
-            Course.objects.create(
-                title=title,
-                category=category,
-                teacher=teacher,
-                video_url=video_url,
-                cover_url=cover_url,
-                price=price,
-                duration=duration,
-                profile=profile
-            )
+        form = request.POST
+        if form:
+            if form.get("lang","cn")=="en":
+                GongShang.objects.filter(
+                    pk=form.get("pk",0)).update(
+                    title_en=title,
+                    desc_en=desc,
+                    content_en=content,
+                    thumbnail_en=thumbnail,
+                    pub_time=pub_time,
+                    category=category)
+            else:
+                News.objects.filter(
+                    pk=pk).update(
+                    title=title,
+                    desc=desc,
+                    thumbnail=thumbnail,
+                    content=content,
+                    pub_time=pub_time,
+                    category=category)
+           
             return restful.ok()
         else:
             return restful.params_error(form.get_error())
---
+++ static/js/edit_company.js
@@ -0,0 +1,65 @@
+
+
+ 
+
+// 文本编辑器
+$(function () {
+    // window.ue 是将ue这个变量作为全局变量来定义
+    window.ue = UE.getEditor('editor',{
+        // 官方文档中给出前端的高度配置说明
+        "initialFrameHeight":400 , //初始化编辑器宽度
+        'serverUrl':'/ueditor/upload/' // 服务器统一请求接口路径
+    });
+});
+
+// 更新企业信息
+$(function () {
+    var submiBtn = $('#submit-btn');
+    submiBtn.click(function (event) {
+        event.preventDefault(); // 去除原有的格式
+        var btn = $(this); // 当前点击的这个按钮
+        var fullName = $("input[name='fullName']").val();
+        var legalPersonName = $("input[name='legalPersonName']").val();
+        var businessScope = $("input[name='businessScope']").val();
+        var phone = $("input[name='phone']").val();
+        var email = $("input[name='email']").val();
+        var address = $("input[name='address']").val();
+        var company_url = $("input[name='company_url']").val();
+        var desc = $("textarea[name='desc']").val();
+    
+        var code = $("input[name='code']").val();
+        var lang = $("input[name='lang']").val();
+      
+        var gs_info_id = btn.attr('data-gs_info-id'); 
+        var url = '/cms/company_edit/';
+      
+        xfzajax.post({
+            'url':url,
+            'data': {
+                'fullName':fullName,
+                'lang':lang, 
+                'legalPersonName':legalPersonName,
+                'businessScope':businessScope,
+                'desc':desc,
+                'phone':phone,
+                'email':email,
+                'address':address,
+                'company_url':company_url,
+                'code':code,
+                'pk':gs_info_id
+            },
+
+            'success':function (result) {
+                if (result['code'] === 200){
+
+                    xfzalert.alertSuccess('企业信息已更新！',function () {
+                        url = '/cms/company_list/';
+                        window.location.reload(); // 重新加载当前界面
+
+                    });
+                }
+            }
+        });
+    });
+});
+
